9c395c77e26892ab59a48aa8d97c7db734aa0a2e
Icaro-Miguel <80598515+Icaro-Miguel@users.noreply.github.com>
2021-05-24 16:00:19 -0300

    adicionando o arquivo na web.
    adicionando o arquivo na web...

---
+++ script.js
@@ -0,0 +1,9 @@
+let a = parseInt(prompt('digite um número para calcular sua potência.'));
+let b = parseInt(prompt('digite outro número. ps: este número irá elevar o primeiro.'))
+
+
+
+console.log(Math.pow(a, b))
+
+
+
